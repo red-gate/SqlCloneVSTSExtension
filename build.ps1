885fb002507eea9d596476096701b2cd3dbd24c1
Praveen Dhanasekaran <Praveen.Dhanasekaran@red-gate.com>
2018-06-11 11:30:04 +0100

    add delete image task

--- build.ps1
+++ build.ps1
@@ -10,6 +10,8 @@ trap
 
 .\jsonSanityCheck.ps1 -manifest "extension-manifest.json" -task "DeleteTask\task.json"
 
+.\jsonSanityCheck.ps1 -manifest "extension-manifest.json" -task "DeleteImageTask\task.json"
+
 
 & npm install
 
@@ -21,9 +23,11 @@ Save-Module -Name VstsTaskSdk -Path .\TaskModules -RequiredVersion 0.8.1 -Force
 & xcopy TaskModules\VstsTaskSdk\0.8.1 ImageTask\ps_modules\VstsTaskSdk /E /y /i
 & xcopy TaskModules\VstsTaskSdk\0.8.1 CloneTask\ps_modules\VstsTaskSdk /E /y /i
 & xcopy TaskModules\VstsTaskSdk\0.8.1 DeleteTask\ps_modules\VstsTaskSdk /E /y /i
+& xcopy TaskModules\VstsTaskSdk\0.8.1 DeleteImageTask\ps_modules\VstsTaskSdk /E /y /i
 
 & xcopy TaskModules\ClonePowerShell ImageTask\modules /E /y /i
 & xcopy TaskModules\ClonePowerShell CloneTask\modules /E /y /i
 & xcopy TaskModules\ClonePowerShell DeleteTask\modules /E /y /i
+& xcopy TaskModules\ClonePowerShell DeleteImageTask\modules /E /y /i
 
 & .\node_modules\.bin\vset.cmd package -m extension-manifest.json -o .\Packages 
